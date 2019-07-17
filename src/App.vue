9c12f5924fbe458723d9c07520ade83586adeb88
froiransi.camico <froiransi@gmail.com>
2019-07-17 14:34:08 +0200

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="(crime,index) in crimes" :key="index">{{crime.Category}}: <strong>{{crime.arrest_count}}</strong></li>
+    </ul>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  //components: {
+   // HelloWorld
+  //},
+  async created(){
+
+    let response = await this.$http.get("http://nflarrest.com/api/v1/crime").then((response) => {
+      this.crimes = response.data
+    })
+  },
+  data(){
+    return{
+      crimes: []
+    }
   }
+
 }
 </script>
 
