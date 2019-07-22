a65abe5abbd85fb4f9edff3f44582ba8aafce655
froiransi.camico <froiransi@gmail.com>
2019-07-22 12:50:07 +0200

    reports by crime

--- src/views/Crimes.vue
+++ src/views/Crimes.vue
@@ -7,33 +7,106 @@
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item font-weight-bold"><a href="/">Home</a></li>
-                <li class="breadcrumb-item" aria-current="page">{{currentCrime}}</li>
+                <li class="breadcrumb-item" aria-current="page">Statistic by crime: {{currentCrime}}</li>
             </ol>
         </nav>
+        <ul class="nav nav-pills nav-fill">
+          <li class="nav-item">
+            <a :class="activeTab('crimesByPlayer')" class="nav-link" @click="changeTab('crimesByPlayer')"  href="#">Top Players for Crime</a>
+          </li>
+          <li class="nav-item">
+            <a :class="activeTab('crimesByTeam')" @click="changeTab('crimesByTeam')" class="nav-link" href="#">Top Teams for Crime</a>
+          </li>
+          <li class="nav-item">
+            <a :class="activeTab('crimesByPosition')" @click="changeTab('crimesByPosition')" class="nav-link" href="#">Top Positions for Crime</a>
+          </li>
+        </ul>
+        <div v-if="currentTab == 'crimesByPlayer'">
+          <p class="px-3 pt-3">Top Players by crime: </p>
+          <ol>
+            <li v-for="crime in crimesByPlayer" v-bind:key="crime.id">{{crime.Name}}: {{crime.arrest_count}}</li>
+          </ol>
+        </div>
+        <div v-if="currentTab == 'crimesByTeam'">
+          <p class="px-3 pt-3">Top Teams by crime: </p>
+          <ol>
+            <li v-for="crime in crimesByTeam" v-bind:key="crime.id">{{crime.Team_city}} {{crime.Team_name}}: {{crime.arrest_count}}</li>
+          </ol>
+        </div>
+        <div v-if="currentTab == 'crimesByPosition'">
+          <p class="px-3 pt-3">Top Positions by crime: </p>
+          <ol>
+            <li v-for="crime in crimesByPosition" v-bind:key="crime.id">{{crime.Position}}: {{crime.arrest_count}}</li>
+          </ol>
+        </div>
     </div>
 </template>
 <script>
 export default {
   async created() {
     const crimeId = this.$route.params.crimeCategory
-    await this.calculateCrimes(crimeId);
+    await this.calculateCrimesByPlayer(crimeId);
+    await this.calculateCrimesByTeam(crimeId);
+    await this.calculateCrimesByPosition(crimeId);
   },
   methods: {
-    async calculateCrimes(crimeId) {
-        let responseCrime = await this.$http.get(`http://nflarrest.com/api/v1/crime/topPlayers/${crimeId}`)
-        this.currentCrime = crimeId;
+    async calculateCrimesByPlayer(crimeId) {
+      this.currentCrime = crimeId;
+      let internalCrimesByPlayer = []
+      let responseCrimePlayer = await this.$http.get(`http://nflarrest.com/api/v1/crime/topPlayers/${crimeId}`)
+      this.crimesByPlayer = responseCrimePlayer.data
+      this.crimesByPlayer.forEach(element => {
+        internalCrimesByPlayer.push(element)
+      });
+      this.crimesByPlayer = internalCrimesByPlayer
+    },
+    async calculateCrimesByTeam(crimeId) {
+      this.currentCrime = crimeId;
+      let internalCrimesByTeam = []
+      let responseCrimeTeam = await this.$http.get(`http://nflarrest.com/api/v1/crime/topTeams/${crimeId}`)
+      this.crimesByTeam = responseCrimeTeam.data
+      this.crimesByTeam.forEach(element => {
+        internalCrimesByTeam.push(element)
+      });
+      this.crimesByPlayerTeam = internalCrimesByTeam
+    },
+    async calculateCrimesByPosition(crimeId) {
+      this.currentCrime = crimeId;
+      let internalCrimesByPosition = []
+      let responseCrimePosition = await this.$http.get(`http://nflarrest.com/api/v1/crime/topPositions/${crimeId}`)
+      this.crimesByPosition = responseCrimePosition.data
+      this.crimesByPosition.forEach(element => {
+        internalCrimesByPosition.push(element)
+      });
+      this.crimesByPlayerPosition = internalCrimesByPosition
+    },
+    changeTab(name){
+      this.currentTab = name
+    },
+    activeTab(tab){
+      if (tab == this.currentTab){
+        return 'active'
+      } else {
+        return ''
+      }
     }
   },
   components: {},
   data() {
     return {
       currentCrime: "",
-      crimes: []
+      currentTab: "crimesByPlayer",
+      crimesByPlayer: [],
+      crimesByTeam: [],
+      crimesByPosition: []
     };
   }
 };
 </script>
 <style>
+.breadcrumb{
+  background-color: transparent;
+}
 </style>
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
     <div class="container main">
-      <header class="header_nfl">
+      <header class="header_nfl  mb-2">
         <img class="icon_nfl" src="../../src/assets/nfl_icon.png">
         <span class="banner_title">NFL Reports</span>
       </header>
+      <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item font-weight-bold"><a href="/">Home</a></li>
+                <li class="breadcrumb-item" aria-current="page">Welcome to the NFL crimes report Website</li>
+            </ol>
+        </nav>
       <div class="container py-2">
         <ul class="nav nav-pills nav-fill">
           <li class="nav-item">
@@ -25,7 +31,6 @@
         </ul>
       </div>
       <div v-if="currentTab == 'home'" class="container">
-        <p class="px-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
         <div class="row">
           <div class="col-md-3 col-12">
             <div class="card mb-3">
@@ -198,7 +203,7 @@ export default {
                 },
                 chart: {
                   width: 350,
-                  height: 400,
+                  height: 600,
                 },
               }
             }
@@ -332,4 +337,119 @@ export default {
 </script>
 
 <style>
+
+#app {
+font-family: 'Sunflower', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #070042;
+}
+
+.main{
+  padding-left: 0px;
+  padding-right: 0px;
+}
+
+.icon_nfl{
+  max-height: 7rem;
+  padding-top: 1rem;
+}
+
+.banner_title{
+  vertical-align: bottom;
+  font-size: 4rem;
+  color: white;
+}
+
+.header_nfl{
+  background-image: url(../../src/assets/banner.png);
+  border: none;
+  height: 8rem;
+  border-radius: 0px 0px 15px 15px;
+  -moz-border-radius: 0px 0px 15px 15px;
+  -webkit-border-radius: 0px 0px 15px 15px;
+  -webkit-background-size: 100% 100%; /* Safari 3.0 */
+  -moz-background-size: 100% 100%; /* Gecko 1.9.2 (Firefox 3.6) */
+  -o-background-size: 100% 100%; /* Opera 9.5 */
+  background-size: 100% 100%;
+}
+
+.nav-link{
+  color: white;
+  background-color: #070042;
+  margin-right: 2px !important;
+  margin-bottom: 2px !important;
+}
+
+.nav-link:hover{
+  color: #070042 !important;
+  background-color: #F2F2F2 !important;
+}
+
+.active, .active:hover{
+  font-weight: 700;
+  background-color: #B5000C !important;
+  color: white!important;
+}
+
+.apexcharts-canvas{
+  right: -15rem !important;
+}
+
+.chart_set{
+  width: 50% !important;
+}
+
+.btn{
+  background-color: #4E0110;
+  border: none;
+}
+
+.btn:hover{
+  background-color: #B5000C;
+}
+
+@media (max-width: 992px) {
+
+  .apexcharts-canvas{
+    right: -10rem !important;
+  }
+
+}
+
+
+@media (max-width: 768px) {
+
+  .apexcharts-canvas{
+    right: -6.5rem !important;
+  }
+
+}
+
+@media (max-width: 454px) {
+
+  .header_nfl{
+    background-image: url(../../src/assets/banner_mobile.png);
+    height: 6rem;
+  }
+
+  .icon_nfl{
+    max-height: 5rem;
+    padding-top: 0.5rem;
+  }
+
+  .banner_title{
+    font-size: 2.5rem;
+  }
+
+  .apexcharts-canvas{
+    right: -2rem !important;
+  }
+
+  .dropdown-toggle, .dropdown-menu{
+    margin-left: 1rem !important;
+  }
+
+
+}
 </style>
